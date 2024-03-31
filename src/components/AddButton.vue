711137cf14eaf1f5be41abc3bd86bfda068ec1e5
Artyom Kim <arti-kim@yandex.ru>
2024-04-01 01:07:34 +1100

    fix: native event in Vue3

--- src/components/AddButton.vue
+++ src/components/AddButton.vue
@@ -1,6 +1,7 @@
 <template>
   <button
     type="button"
+    @click="$emit('click')"
     :disabled="disabled"
     class="inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
     :class="{
@@ -32,6 +33,10 @@ export default {
       required: false,
       default: false,
     },
+  },
+
+  emits: {
+    click: null,
   },
 };
 </script>
